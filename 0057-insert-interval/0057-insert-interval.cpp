30d66c5f1ec19cef01ecf3933d15140b0d0e5c7f
Priyanshu Rai <70860041+priyanshurai007@users.noreply.github.com>
2025-10-28 13:31:14 +0530

    Time: 0 ms (100%), Space: 21.9 MB (21.44%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        //intution three case possible non-overlap || overlap || non-overlap
+
         int n=intervals.size();
         vector<vector<int>> result;
-        int i=0;
-        while(i<n && intervals[i][1]<newInterval[0]){
-            result.push_back(intervals[i]);
-            i++;
-        }
-        //overlapping maintain the range.
-        while(i<n && intervals[i][0]<=newInterval[1]){
-            newInterval[0]=min(newInterval[0],intervals[i][0]);
-            newInterval[1]=max(newInterval[1],intervals[i][1]);
-            i++;
-        }
-        result.push_back(newInterval);
+        if(n==0) return {newInterval};
 
-        //add remaining non-overlapping intervals.
-        while(i<n){
-            result.push_back(intervals[i]);
-            i++;
+        for(int i=0; i<n; i++){
+            while(i<n && intervals[i][1]<newInterval[0]){
+                result.push_back(intervals[i]);
+                i++;
+            }
+
+            //overlapping found lets maintain the new interval till overlapping.
+            //overlapping tabtak rahegi jabtak ith start<=newIntervalend milta rahega.
+            while(i<n && intervals[i][0]<=newInterval[1]){
+                newInterval[0]=min(intervals[i][0], newInterval[0]);
+                newInterval[1]=max(intervals[i][1], newInterval[1]);
+                i++;
+            }
+            result.push_back({newInterval[0],newInterval[1]});
+
+            while(i<n){
+                result.push_back(intervals[i]);
+                i++;
+            }
         }
-        
+
         return result;
     }
 };
