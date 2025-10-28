5915469ffed0a0305ce2a449ff1fdca2fe79aad3
Priyanshu Rai <70860041+priyanshurai007@users.noreply.github.com>
2025-10-28 10:44:21 +0530

    Time: 0 ms (100%), Space: 22 MB (96.96%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -13,36 +13,73 @@ public:
 
         //brute force.
 
-        vector<int> left(n);
-        vector<int> right(n);
-        left[0]=1;
-        right[n-1]=1;
+        // vector<int> left(n);
+        // vector<int> right(n);
+        // left[0]=1;
+        // right[n-1]=1;
 
-        //first distribute the left way along.
+        // //first distribute the left way along.
         
-        for(int i=1; i<n; i++){
-            if(ratings[i]>ratings[i-1]){
-                left[i]=left[i-1]+1;
-            }else{
-                left[i]=1;
+        // for(int i=1; i<n; i++){
+        //     if(ratings[i]>ratings[i-1]){
+        //         left[i]=left[i-1]+1;
+        //     }else{
+        //         left[i]=1;
+        //     }
+        // }
+
+        // //second distribut the right way along.
+
+        // for(int i=n-2; i>=0; i--){
+        //     if(ratings[i]>ratings[i+1]){
+        //         right[i]=right[i+1]+1;
+        //     }else{
+        //         right[i]=1;
+        //     }
+        // }
+
+        // //take the max of both as you have to satisfy both the left and right way. as we already distributed the in minimum way so there will be no problem.
+
+        // int sum=0;
+        // for(int i=0; i<n; i++){
+        //     sum+=max(left[i],right[i]);
+        // }
+
+        // return sum;
+
+        /*
+           Intution: for optimal way is slope based.
+        */
+
+        int sum=1; //as i am moving from the second i=1 element
+        int i=1;
+
+        while(i<n){
+            if(ratings[i]==ratings[i-1]){
+                sum++;
+                i++;
+                continue;
             }
-        }
 
-        //second distribut the right way along.
+            int peak=1;
 
-        for(int i=n-2; i>=0; i--){
-            if(ratings[i]>ratings[i+1]){
-                right[i]=right[i+1]+1;
-            }else{
-                right[i]=1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                peak++;
+                sum+=peak;
+                i++;
             }
-        }
 
-        //take the max of both as you have to satisfy both the left and right way. as we already distributed the in minimum way so there will be no problem.
+            int down=1;
 
-        int sum=0;
-        for(int i=0; i<n; i++){
-            sum+=max(left[i],right[i]);
+            while(i<n && ratings[i]<ratings[i-1]){
+                sum+=down;
+                down++;
+                i++;
+            }
+
+            if(down>peak){
+                sum+=down-peak;
+            }
         }
 
         return sum;
